dcacf038853b0ccb6a0aae9c867b281a60d2d2e1
Devl0ck <dimaogorodov@mail.ru>
2023-08-19 22:31:14 +0300

    v0.2

--- Calories.py
+++ Calories.py
@@ -1,38 +1,49 @@
-def get_user_input(prompt, is_numeric=False):  # Проверка на правильность введенных данных
+def get_valid_input(prompt, validation_func):
     while True:
         user_input = input(prompt)
-        if is_numeric:
-            try:
-                user_input = float(user_input)
-                return user_input
-            except ValueError:
-                print("Введено не число. Попробуйте снова.")
-                continue
-        if user_input:
+        if validation_func(user_input):
             return user_input
-        print("Поле не может быть пустым. Попробуйте снова.")
+        print("Некорректный ввод. Попробуйте снова.")
+
+
+def is_numeric_input(user_input):
+    return user_input.replace('.', '', 1).isdigit()
+
+
+def is_positive_numeric_input(user_input):
+    return is_numeric_input(user_input) and float(user_input) > 0
+
+
+def is_valid_gender(user_input):
+    return user_input.lower() in ["мужской", "женский"]
+
+
+def is_valid_activity_level(user_input):
+    return user_input in ["1", "2", "3", "4", "5"]
 
 
 # Ввод данных
-while True:
-    gender = get_user_input("Введите свой пол (мужской, женский, другой): ")
-    if gender.lower() in ["мужской", "женский", "другой"]:
-        break
-    print("Некорректный пол. Попробуйте снова.")
+gender = get_valid_input("Введите свой пол (мужской, женский): ", is_valid_gender)
+age = float(get_valid_input("Введите свой возраст (полных лет): ", is_positive_numeric_input))
+weight = float(get_valid_input("Введите свой вес (кг): ", is_positive_numeric_input))
+height = float(get_valid_input("Введите свой рост (см): ", is_positive_numeric_input))
+activity_level = get_valid_input("Введите уровень активности (1-5): ", is_valid_activity_level)
 
-age = get_user_input("Введите свой возраст: ", is_numeric=True)
-weight = get_user_input("Введите свой вес: ", is_numeric=True)
-height = get_user_input("Введите свой рост: ", is_numeric=True)
+# Подсчет количества калорий
+activity_level_mapping = {"1": 1.2, "2": 1.375, "3": 1.55, "4": 1.725, "5": 1.9}
+activity_level = activity_level_mapping[activity_level]
 
-while True:
-    activity_level = get_user_input("Введите уровень активности (сидячий, умеренный, активный): ")
-    if activity_level.lower() in ["сидячий", "умеренный", "активный"]:
-        break
-    print("Некорректный уровень активности. Попробуйте снова.")
+if gender == "мужской" and 13 <= age < 80:
+    amount_calories = (10 * weight + 6.25 * height - 5 * age + 5) * int(activity_level)
+elif gender == "женский" and 13 <= age < 80:
+    amount_calories = (10 * weight + 6.25 * height - 5 * age - 161) * int(activity_level)
+else:
+    print("Выход из программы, так как данные некорректны")
+    exit()
 
 # Вывод данных
 print("Пол:", gender)
 print("Возраст:", age)
 print("Вес:", weight)
 print("Рост:", height)
-print("Уровень активности:", activity_level)
+print("Необходимое количество килокалорий (ккал) в сутки: ", amount_calories)
